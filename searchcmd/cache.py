0eba80b6568a17d4d4c3f862b3a69cf967764a35
Jimmy Petersson <jimmy@tajitsu.com>
2015-05-03 08:05:44 +0200

    bug fixes

--- searchcmd/cache.py
+++ searchcmd/cache.py
@@ -1,9 +1,10 @@
 import os
 import hashlib
 import json
+import errno
 
 from commands import Commands, Command
-from download import HtmlDocument
+from download import HtmlDocument, Url
 
 CACHE_DIR = os.path.sep.join(['', 'tmp', 'searchcmd'])
 try:
@@ -13,8 +14,13 @@ except OSError:
 
 
 def get(**args):
-    with open(get_file_name(**args)) as inp:
-        return json.load(inp, cls=CustomTypeDecoder)
+    try:
+        with open(get_file_name(**args)) as inp:
+            return json.load(inp, cls=CustomTypeDecoder)
+    except IOError as e:
+        if e.errno == errno.ENOENT:
+            return None
+        raise
 
 
 def store(commands, **args):
@@ -26,9 +32,8 @@ def get_file_name(**args):
     return os.path.join(CACHE_DIR, hashlib.sha1(repr(args.items())).hexdigest())
 
 
-TYPES = {'Commands': Commands,
-         'Command': Command,
-         'HtmlDocument': HtmlDocument}
+TYPES = {cls.__name__: cls for cls in [
+    Commands, Command, HtmlDocument, Url]}
 
 # TODO: Give credit to stackoverflow answer
 class CustomTypeEncoder(json.JSONEncoder):
@@ -52,13 +57,17 @@ class CustomTypeDecoder(json.JSONDecoder):
     def decode(self, s):
 
         def load_objects(obj):
-            if not isinstance(obj, dict):
+            if isinstance(obj, list):
+                return [load_objects(e) for e in obj]
+            elif isinstance(obj, dict):
+                if len(obj) == 1:
+                    type_name, value = obj.items()[0]
+                    type_name = type_name.strip('_')
+                    if type_name in TYPES:
+                        return TYPES[type_name].from_dict(load_objects(value))
+                return {key: load_objects(value)
+                        for key, value in obj.iteritems()}
+            else:
                 return obj
-            if len(obj) == 1:
-                type_name, value = obj.items()[0]
-                type_name = type_name.strip('_')
-                if type_name in TYPES:
-                    return TYPES[type_name].from_dict(load_objects(value))
-            return {key: load_objects(value) for key, value in obj.iteritems()}
 
         return load_objects(json.JSONDecoder.decode(self, s))
